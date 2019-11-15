1c36579a2166f02e308352ba4cc9b68f9041ea0e
Jamie Berrier <berrier.jamie@gmail.com>
2019-11-15 18:58:34 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,9 @@ class Post < ActiveRecord::Base
 
   belongs_to :author
   validate :is_title_case
-
+  # Whenever you are modifying an attribute of the model,
+  # use before_validation. If you are doing some other action,
+  # then use before_save.
   before_validation :make_title_case
 
   private
